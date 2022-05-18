18f28d15c8d39361d47c5619c4d34c5e8cf74281
romanovich_anton <anton.romanovich21@gmail.com>
2022-05-18 23:48:01 +0300

    feat: 1st task done

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 const location = path.join(__dirname, 'text.txt'); 
 
-const readableStream = fs.createReadStream(location, "utf-8");
+const readableStream = fs.createReadStream(location, 'utf-8');
 
 readableStream.on('data', chunk => console.log(chunk));
-readableStream.on('end', () => console.log("THAT'S IT"));
+readableStream.on('end', () => console.log('THAT IS IT'));

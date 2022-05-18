a630cd3943b1257375f042e522a4f26cb2d7250b
romanovich_anton <anton.romanovich21@gmail.com>
2022-05-18 23:48:52 +0300

    feat: 2nd task done

---
+++ 02-write-file/index.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const fs = require('fs');
+
+const { stdin, stdout } = process; 
+
+fs.writeFile(path.join(__dirname, 'text.txt'), '', err => {
+  if(err) throw err;
+});
+
+stdout.write('Howdy!!\nHow can I help you?\n');
+stdin.on('data', data => {
+  if(data.toString().trim() == 'exit') {
+    stdout.write('Looking forward to seeing you soon again');
+    process.exit();
+  }
+  fs.appendFile(path.join(__dirname, 'text.txt'), 
+    data.toString(),
+    err => {
+      if(err) throw err;
+    });
+});
+
+process.on('SIGINT', () => {
+  stdout.write('Looking forward to seeing you soon');
+  process.exit();
+});

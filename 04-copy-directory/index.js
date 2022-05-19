a63258d2a2a6370cba5b7c1ff1ab206784ce261d
romanovich_anton <anton.romanovich21@gmail.com>
2022-05-19 22:20:50 +0300

    fix: task 4 add clear folder before copy

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,6 +4,25 @@ const path = require('path');
 fsPromises.mkdir(path.join(__dirname, 'files-copy'), {recursive: true})
   .then();
 
+try {
+  fsPromises.access(path.join(__dirname, 'files-copy')).then();
+  fsPromises.readdir(path.join(__dirname, 'files-copy'), {withFileTypes: true})
+    .then(data => {
+      data.forEach(file => {
+        fsPromises.unlink(path.join(__dirname, 'files-copy', file.name), err => {
+          console.log(err);
+        });
+      });
+    });
+} catch {
+  fsPromises.readdir(path.join(__dirname, 'files'), {withFileTypes: true})
+    .then(data => {
+      data.forEach(file => {
+        fsPromises.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, path.join('files-copy', file.name)));
+      });
+    });
+}
+
 fsPromises.readdir(path.join(__dirname, 'files'), {withFileTypes: true})
   .then(data => {
     data.forEach(file => {

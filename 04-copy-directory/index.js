c15041cfa1e9e8d325be4100b49c5c896d39d1a2
romanovich_anton <anton.romanovich21@gmail.com>
2022-05-19 21:36:11 +0300

    feat: task 4 done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,12 @@
+const fsPromises = require('fs/promises');
+const path = require('path');
 
+fsPromises.mkdir(path.join(__dirname, 'files-copy'), {recursive: true})
+  .then();
+
+fsPromises.readdir(path.join(__dirname, 'files'), {withFileTypes: true})
+  .then(data => {
+    data.forEach(file => {
+      fsPromises.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, path.join('files-copy', file.name)));
+    });
+  });
